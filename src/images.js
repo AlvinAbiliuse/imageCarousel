067b7d8756d3455867f468706e80192115323b8d
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-23 09:57:16 +0400

    added images.js

---
+++ src/images.js
@@ -0,0 +1,23 @@
+import cat1 from "./images/cat1.jpg";
+import cat2 from "./images/cat2.jpg";
+import cat3 from "./images/cat3.jpg";
+import cat4 from "./images/cat4.jpg";
+import cat5 from "./images/cat5.jpg";
+import cat6 from "./images/cat6.jpg";
+
+import dog1 from "./images/dog1.jpg";
+import dog2 from "./images/dog2.jpg";
+import dog3 from "./images/dog3.jpg";
+import dog4 from "./images/dog4.jpg";
+import dog5 from "./images/dog5.jpg";
+import dog6 from "./images/dog6.jpg";
+
+
+
+
+
+
+
+export const cat = [ cat1, cat2, cat3, cat4, cat5, cat6 ]; 
+
+export const dog = [ dog1, dog2, dog3, dog4, dog5, dog6 ];
