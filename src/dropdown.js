56bded6adc5bb892a201518589887888d1c62f7b
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-24 02:03:36 +0400

    moved dropdown button queryselector and eventlistener to dropdown.js

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,6 +1,13 @@
-export function dropdown() {
+function dropdownToggle() {
 	document.querySelector(".dropDown ul").classList.toggle(
 		"show");
-
 }
+
+
+export function dropdown() {
+	let dropDownBtn = document.querySelector(".dropDownButton");
+	dropDownBtn.addEventListener("click", dropdownToggle);
+};
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-
 import { dropdown } from "./dropdown.js";
 import { cat, dog } from "./exports.js";
 import { carousel } from "./carousel.js";
-console.log("hello world");
 
-let dropDownBtn = document.querySelector(".dropDownButton");
 for (let i in cat) {
 	console.log(cat[i]);
 };
 
-dropDownBtn.addEventListener("click", dropdown);
+dropdown();
