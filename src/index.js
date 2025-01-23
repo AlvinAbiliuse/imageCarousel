d6392fa8e6738df8153daa1e2df650c3fedaf60b
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-23 08:22:38 +0400

    added dropdown module

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./styles.css";
-
+import { dropdown } from "./dropdown.js";
 
 console.log("hello world");
+
+let dropDownBtn = document.querySelector(".dropDownButton");
+
+dropDownBtn.addEventListener("click", dropdown);
