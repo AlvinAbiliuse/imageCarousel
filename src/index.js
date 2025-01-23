493233390d9ae7545d9b0e1e77a8d72bdd730183
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-24 01:21:51 +0400

    added transition to ul transform scale

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
+import "./dropdownStyles.css";
+// import "./carouselStyles.css";
+
+
 import { dropdown } from "./dropdown.js";
 import { cat, dog } from "./images.js";
-
+import { carousel } from "./carousel.js";
 console.log("hello world");
 
 let dropDownBtn = document.querySelector(".dropDownButton");
