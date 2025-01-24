d8bb3eed09ac26e7b94c8755c8371537a9c7f7c7
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-24 13:23:49 +0400

    index.js linting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import { dropdown } from "./dropdown.js";
-import { cat, dog } from "./exports.js";
 import { carousel } from "./carousel.js";
-
-for (let i in cat) {
-  console.log(cat[i]);
-}
-
-let data = { Cats: cat, Dogs: dog };
 
 dropdown();
 carousel();
